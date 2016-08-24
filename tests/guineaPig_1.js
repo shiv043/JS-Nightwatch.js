9911b198c17920f22c84c7f55785a25fd83bd655
Liam Newman <bitwiseman@gmail.com>
2016-08-24 06:53:01 -0700

    Prep: Added more tests

--- tests/guineaPig_1.js
+++ tests/guineaPig_1.js
@@ -5,18 +5,39 @@ module.exports = {
 
     '@tags': ['guineaPig'],
 
-    'Guinea Pig Assert Title 2': function(client) {
+    'Guinea Pig Assert Title 1 - A': function(client) {
+        client
+            .url('https://saucelabs.com/test/guinea-pig')
+            .waitForElementVisible('body', 1000)
+            .assert.title('I am a page title - Sauce Labs - A');
+    },
+
+    'Guinea Pig Assert Title 1 - B': function(client) {
+        client
+            .url('https://saucelabs.com/test/guinea-pig')
+            .waitForElementVisible('body', 1000)
+            .assert.title('I am a page title - Sauce Labs');
+    },
+
+    'Guinea Pig Assert Title 1 - C': function(client) {
         client
             .url('https://saucelabs-sample-test-frameworks.github.io/training-test-page')
             .waitForElementVisible('body', 1000)
             .assert.title('I am a page title - Sauce Labs');
     },
 
-    'Failure Guinea Pig Assert Title 2': function(client) {
+    'Guinea Pig Assert Title 1 - D': function(client) {
         client
             .url('https://saucelabs-sample-test-frameworks.github.io/training-test-page')
             .waitForElementVisible('body', 1000)
-            .assert.title('Banana');
+            .assert.title('I am a page title - Sauce Labs');
+    },
+
+    'Guinea Pig Assert Title 1 - E': function(client) {
+        client
+            .url('https://saucelabs.com/test/guinea-pig')
+            .waitForElementVisible('body', 1000)
+            .assert.title('I am a page title - Sauce Labs');
     },
 
     afterEach: function(client, done) {
