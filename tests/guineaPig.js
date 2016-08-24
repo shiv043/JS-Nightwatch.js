dd9a8d31e64c6c5b7aef5dedc64545525075662b
Liam Newman <bitwiseman@gmail.com>
2016-08-24 06:53:01 -0700

    Prep: Added more tests

--- tests/guineaPig.js
+++ tests/guineaPig.js
@@ -5,7 +5,35 @@ module.exports = {
 
     '@tags': ['guineaPig'],
 
-    'Guinea Pig Assert Title': function(client) {
+    'Guinea Pig Assert Title 0 - A': function(client) {
+        client
+            .url('https://saucelabs.com/test/guinea-pig')
+            .waitForElementVisible('body', 1000)
+            .assert.title('I am a page title - Sauce Labs');
+    },
+
+    'Guinea Pig Assert Title 0 - B': function(client) {
+        client
+            .url('https://saucelabs.com/test/guinea-pig')
+            .waitForElementVisible('body', 1000)
+            .assert.title('I am a page title - Sauce Labs');
+    },
+
+    'Guinea Pig Assert Title 0 - C': function(client) {
+        client
+            .url('https://saucelabs.com/test/guinea-pig')
+            .waitForElementVisible('body', 1000)
+            .assert.title('I am a page title - Sauce Labs');
+    },
+
+    'Guinea Pig Assert Title 0 - D': function(client) {
+        client
+            .url('https://saucelabs.com/test/guinea-pig')
+            .waitForElementVisible('body', 1000)
+            .assert.title('I am a page title - Sauce Labs');
+    },
+
+    'Guinea Pig Assert Title 0 - E': function(client) {
         client
             .url('https://saucelabs-sample-test-frameworks.github.io/training-test-page')
             .waitForElementVisible('body', 1000)
