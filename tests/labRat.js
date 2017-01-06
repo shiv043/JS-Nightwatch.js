45e3f6f2c93bb40548defe9d5f1b67fb7af877f4
Liam Newman <bitwiseman@gmail.com>
2017-01-06 12:32:06 -0800

    Fixed bad title string

--- tests/labRat.js
+++ tests/labRat.js
@@ -16,7 +16,7 @@ module.exports = {
         client
             .url('https://saucelabs-sample-test-frameworks.github.io/training-test-page')
             .waitForElementVisible('body', 1000)
-            .assert.title('Banana');
+            .assert.title('I am a page title - Sauce Labs');
     },
 
     afterEach: function(client, done) {
